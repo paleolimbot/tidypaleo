0557f45d6e814aa22848216b805849a6a6e7bf0d
Dewey Dunnington <dewey@fishandwhistle.net>
2018-07-19 13:26:17 -0400

    move age depth info to same file

--- tests/testthat/test-sec_axis.R
+++ tests/testthat/test-sec_axis.R
@@ -6,26 +6,26 @@ test_that("creating a second axis object", {
   )
 
   # depth as the primary axis
-  expect_true(ggplot2::is.ggproto(as_sec_axis(alta_lake_adm)))
-  expect_is(as_sec_axis(alta_lake_adm), "AxisSecondary")
+  expect_true(ggplot2::is.ggproto(age_depth_as_sec_axis(alta_lake_adm)))
+  expect_is(age_depth_as_sec_axis(alta_lake_adm), "AxisSecondary")
 
   expect_silent(
     ggplot2::ggplot(alta_lake_210Pb_ages, ggplot2::aes(y = depth_cm, x = age_year_ad)) +
       ggplot2::geom_line() +
       ggplot2::geom_point() +
-      ggplot2::scale_y_reverse(sec.axis = as_sec_axis(alta_lake_adm))
+      ggplot2::scale_y_reverse(sec.axis = age_depth_as_sec_axis(alta_lake_adm))
   )
 
   # age as the primary axis
-  expect_true(ggplot2::is.ggproto(as_sec_axis(alta_lake_adm, primary = "age")))
-  expect_is(as_sec_axis(alta_lake_adm, primary = "age"), "AxisSecondary")
+  expect_true(ggplot2::is.ggproto(age_depth_as_sec_axis(alta_lake_adm, primary = "age")))
+  expect_is(age_depth_as_sec_axis(alta_lake_adm, primary = "age"), "AxisSecondary")
 
   expect_silent(
     ggplot2::ggplot(alta_lake_210Pb_ages, ggplot2::aes(y = depth_cm, x = age_year_ad)) +
       ggplot2::geom_line() +
       ggplot2::geom_point() +
       ggplot2::scale_y_reverse() +
-      ggplot2::scale_x_continuous(sec.axis = as_sec_axis(alta_lake_adm, primary = "age"))
+      ggplot2::scale_x_continuous(sec.axis = age_depth_as_sec_axis(alta_lake_adm, primary = "age"))
   )
 
 })
